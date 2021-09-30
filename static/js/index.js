ffaf00e99db8ae9993f7d555e5897a5baa8d2230
FranklinSh191 <82122922+FranklinSh191@users.noreply.github.com>
2021-09-30 08:23:50 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,34 +1,42 @@
 //https://www.eclipse.org/paho/clients/js/
 
+function enviarmsg() {
+  console.log("Mensaje enviado");
+  var texto = document.getElementById('enviarmensaje').value;
+  message = new Paho.MQTT.Message(texto);
+  message.destinationName = "jhoni191sh@gmail.com/t2";
+  client.send(message);
+  document.getElementById('enviarmensaje').value = '';
+}
 function LED1_On() {
-	//alert("led on");
-	console.log("led on");
-	//document.getElementById("sensor").innerHTML="led on";
+	console.log("Led Encendido");
+ 	document.getElementById('estado').innerHTML='Encendido';
+	//document.getElementById('led').src='/static/images/On.png';
+	
 	message = new Paho.MQTT.Message("ON");
     	message.destinationName = "jhoni191sh@gmail.com/t2";
     	client.send(message);
   
 }
 function LED1_Off(){	
-	//alert("led off");
-	console.log("led off");
-	message = new Paho.MQTT.Message("OFF");
+	console.log("Led Apagado");
+	document.getElementById('estado').innerHTML='Apagado';
+	//document.getElementById('led').src='/static/images/Off.png';
+	
+  message = new Paho.MQTT.Message("OFF");
     	message.destinationName = "jhoni191sh@gmail.com/t2";
     	client.send(message);
-	//document.getElementById("sensor").innerHTML="led off";
 }
 
-
-
-
-
-
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
+function Leer_sensores(){	
+	console.log("Leyendo Sensores");
+	message = new Paho.MQTT.Message("Leyendo Sensores");
+    	message.destinationName = "jhoni191sh@gmail.com/t2";
+    	client.send(message);
+ 	
+}
   
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
-  // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
@@ -38,37 +46,51 @@ function LED1_Off(){
     onSuccess:onConnect,
     onFailure:doFail
   }
-
   // connect the client
   client.connect(options);
    
   // called when the client connects
   function onConnect() {
     // Once a connection has been made, make a subscription and send a message.
-    console.log("Conectado...");
-	
+    console.log("Usted esta Conectado");
+    alert("Usted se ha Conectado");
     client.subscribe("jhoni191sh@gmail.com/t1");
-    message = new Paho.MQTT.Message("hola saludos a todos");
-    message.destinationName = "jhoni191sh@gmail.com/t2";
+    client.subscribe("jhoni191sh@gmail.com/t2");
+    message = new Paho.MQTT.Message("Hola Conectado desde la Web");
+    message.destinationName = "asilva.fie@unach.edu.ec/test1";
     client.send(message);
 	
   }
-
   function doFail(e){
     console.log(e);
 	
   }
-
   // called when the client loses its connection
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
     }
   }
-
-  // called when a message arrives
+  // para resivir algun mensaje
   function onMessageArrived(message) {
-    console.log("onMessageArrived:"+message.payloadString);
-	  document.getElementById("sensor").innerHTML=message.payloadString;
+	  console.log("Mensaje Recibido: "+message.payloadString);
+	  const separador = ':';
+	  const m1 = 'Led se mantiene Encendido';
+	  const m2 = 'Led se mantiene Apagado';
+	  mensaje = message.payloadString;
+	  const mensajesep = mensaje.split(':');
+	  if(mensaje.includes(separador)){
+		  document.getElementById("sensor1").innerHTML='Estado : '+mensajesep[1];
+		  document.getElementById("sensor2").innerHTML='Estado : '+mensajesep[2];
+		  document.getElementById("recibido").innerHTML='Estado Led - '+mensajesep[0];
+		  
+	  } else if (message.payloadString == "ON") {
+		  console.log("Encendido");	  
+	  } else if (message.payloadString == "OFF") {
+		  console.log("Apagado");	 
+	  } else {
+		  document.getElementById("recibido").innerHTML=message.payloadString;
+	  }
+
   }
   
